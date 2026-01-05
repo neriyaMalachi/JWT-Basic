3ebb8a7153bbc5793641eb849bb159e06398801c
Neriya Malachi <neriamalachi@gmail.com>
2026-01-05 08:41:58 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 8000;
+const SECRET = "my_super_secret_key";
+
+const users = [
+  { username: "dana", password: "1234" },
+  { username: "admin", password: "admin" },
+];
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (!user) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  // יצירת JWT
+  const token = jwt.sign({ username: user.username }, SECRET, {
+    expiresIn: "1h",
+  });
+
+  res.json({ token });
+});
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Missing token" });
+  }
+  try {
+    const decoded = jwt.verify(authHeader, SECRET);
+    console.log(decoded);
+    
+    req.user = decoded; // שומרים מידע על המשתמש
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+}
+
+app.get("/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "You are inside protected route",
+    user: req.user,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
